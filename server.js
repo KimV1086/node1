b9fe1a6da2c5e98166f516c6c06029d3b5182912
Vitalik Kim <kimv1086@gmail.com>
2020-01-29 00:40:35 +0600

    fix

--- server.js
+++ server.js
@@ -16,12 +16,14 @@ const Vigenere = require('caesar-salad').Vigenere;
 const password = "admin";
 
 app.get('/encode/:pass', (req, res) => {
-    let pass = Vigenere.Cipher(password).crypt(req.params.pass);
+    let code = req.params.pass;
+    let pass = Vigenere.Cipher(password).crypt(code);
     res.send(pass);
 });
 
 app.get('/decode/:pass', (req, res) => {
-    let pass = Vigenere.Decipher(password).crypt(req.params.pass);
+    let code = req.params.pass;
+    let pass = Vigenere.Decipher(password).crypt(code);
     res.send(pass);
 });
 
