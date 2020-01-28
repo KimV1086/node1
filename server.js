1c9cfc75c2e97786759dcd385087eb86976f8d78
Vitalik Kim <kimv1086@gmail.com>
2020-01-29 00:09:53 +0600

    add task2

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
 // task1
 
 app.get('/:name', (req, res) => {
     res.send(req.params.name);
+});
+
+// task2
+
+const Vigenere = require('caesar-salad').Vigenere;
+
+const password = "admin";
+
+app.get('/encode/:pass', (req, res) => {
+    let pass = Vigenere.Cipher(password).crypt(req.params.pass);
+    res.send(pass);
+});
+
+app.get('/decode/:pass', (req, res) => {
+    let pass = Vigenere.Decipher(password).crypt(req.params.pass);
+    res.send(pass);
 });
 
 app.listen(port, () => {
